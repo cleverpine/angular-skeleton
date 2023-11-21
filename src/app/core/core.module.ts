f6151800b96977d5266728008c136b9598707a84
Ivan Nikolov <ivan.nikolov@cleverpine.com>
2023-11-21 15:50:14 +0200

    Add header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeaderModule } from 'cp-angular-header';
 
 import {
   TranslateLoader,
@@ -66,6 +67,7 @@ export function initializeApp(
         deps: [HttpClient],
       },
     }),
+    HeaderModule
   ],
   exports: [
     NotFoundPageComponent,
